dcf27c529ce37335ebc9a120867caf6ca2a30153
MdSahilAlii <aksahil18@gmail.com>
2024-06-29 17:19:29 +0530

    JS

---
+++ conditions.js
@@ -0,0 +1,16 @@
+n=344;
+
+if(n % 2=== 0 ){
+    console.log("Even" );
+
+    something ="Hello";
+    let x='nice';
+    const pi=3.14;
+    // pi=5;
+}
+else{
+    console.log("Odd");
+}
+//  console.log(x);
+console.log(something);
+// console.log(pi);
---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello JS");
+
+console.log("JavaScript is Awesome");
+
+a =234;
+console.log(a);
+console.log(typeof(a));
+
+myname ="Sahil" 
+console.log(myname);
+
+console.log(typeof myname);
+
+isLogin=true;
+
+console.log(isLogin);
+
+// exponent
+console.log(2 ** 4);
+
+console.log(1 == '1');
+
+// For DataType
+console.log(1=='1');
+
+console.log(34 + '45');
+
+console.log(11 + '11' + 11);
+
+console.log(null + '123');
+
+
