5771d372a5b18390ece5856b18a38db577efab0d
MdSahilAlii <aksahil18@gmail.com>
2024-06-30 12:51:45 +0530

    Javascript

---
+++ JSques.js
@@ -0,0 +1,62 @@
+// QUES-1
+a=77;
+
+if(a>=70 && a<=120){
+    console.log("The no. lies btw 70 and 120")
+}
+else{
+    console.log("Does not lies btw 70 and 120");
+}
+
+// QUES-2
+age=20;
+if(age>=18){
+    console.log("You are eligible for DL")
+}
+else{
+    console.log("Not Eligible")
+}
+
+// QUES-3
+x=20;
+y=7;
+z=100;
+
+if(x>y){
+    if(x>z){
+        console.log("X is Greater");
+
+    }
+    else{
+        console.log("Z is Greater");
+    }
+}
+else{
+    if(y>z){
+        console.log("Y is Greater");
+    }
+    else{
+        console.log("Z is Greater");
+    }
+}
+
+// QUES-4
+
+// square=64;
+
+// if(typeof(square)=== )
+
+// QUES-5
+
+num=49;
+
+if(num%7 ==0){
+    console.log("Divisible by 7")
+
+}
+else if(num%11 ==0){
+    console.log("Divisible by 11");
+}
+else{
+    console.log("Not Divisble by 7 and 11");
+}
